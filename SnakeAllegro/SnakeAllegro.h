080fc3697831b6c0262184ffd2329cc3ea948ba7
MarkSwierkot <markswierkot@gmail.com>
2018-12-24 16:31:07 +0100

    Special apples adden

--- SnakeAllegro/SnakeAllegro.h
+++ SnakeAllegro/SnakeAllegro.h
@@ -2,13 +2,20 @@
 const int					DISPLAY_WIDTH  = 500;
 const int					DISPLAY_HEIGHT = 500;
 int snakeSize = 10;
-const float					FPS = 60.0;
+ float					FPS = 60.0;
 bool						GAME = true;
 int							targetY = 0;											  
 int							targetX = 0;
-int							movementVariable = 2;											 
+int							movementVariable = 3;											 
 int							result = 0;
 enum						Direction { UP, DOWN, LEFT, RIGHT };
 Direction					snakeDirection = RIGHT;
 
 bool collisionOff;
+int cooldown = 0;
+
+int Rcolor, Gcolor, Bcolor;
+int specialApples = 0;
+int specialAppleTimer = 300;
+ 
+bool specialAppleSpawned = false;
